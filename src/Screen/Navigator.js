eaa89f9cdd39ff0f0348aa1260674bbf202f98f4
Dave Lee <ysangy@gmail.com>
2019-07-01 09:25:18 -0400

    fix bottom tab

--- src/Screen/Navigator.js
+++ src/Screen/Navigator.js
@@ -31,15 +31,18 @@ import {
     Detail,
   });
   
-  const MainTab = createBottomTabNavigator({
+  // 이건 하단에 Tab을 나타 내는 것
+  const BottomTab = createBottomTabNavigator({
     FirstTabStack,
     SecondTab,
     ThirdTab,
   });
   
+// 뷰 위에 다른 뷰를 쌓아 놓는 내비게이터 
+// 예를 들어 Tab Navigator 위에서 다른 View 를 표시 할때 사용
   const MainNavi = createStackNavigator({
     MainTab: {
-      screen: MainTab,
+      screen: BottomTab,
       navigationOptions: ({ navigation }) => ({
         header: null,
       }),
@@ -47,6 +50,8 @@ import {
     FullDetail,
   });
   
+  // createAppContainer 이름 그대로 내비게이터를 관장하는 컨테이너
+  // createSwitchNavigator 이것도 역시 이름 그대로 2가지의 경우 Switch 한다.
   export default createAppContainer(
     createSwitchNavigator(
       {
